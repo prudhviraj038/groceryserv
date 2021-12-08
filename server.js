a2daa5a37c3b6dd31a86d9b65633c21ce4ad37e3
prudhviraj038 <prudhviraj038@gmail.com>
2021-12-09 01:02:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ console.log('Server running at http://127.0.0.1:8081/');
 
 app.get('/products', function (req, res) {
 var url = process.env.MONGODB_URI;
-    var id = req.query.id;
     res.end('hello');
 
 
