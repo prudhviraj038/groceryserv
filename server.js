6bee37cedec3c45cf876a731219da07eb337b149
prudhviraj038 <prudhviraj038@gmail.com>
2022-02-13 16:50:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -258,6 +258,83 @@ app.post('/users/login', function (req, res) {
                 
             }
         })
+
+app.get('/address', function (req, res) {
+var url = process.env.MONGODB_URI;
+    var id = req.query.id;
+    if(id){
+        MongoClient.connect(url, function(err, db) {
+          if (err) throw err;
+          var dbo = db.db("groceryapp");
+            dbo.collection("address").find({"_id": mongodb.ObjectId(id)}).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.end(JSON.stringify(result));
+            db.close();
+          });
+        });
+        
+    }
+    else{
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("groceryapp");
+  dbo.collection("address").find({}).toArray(function(err, result) {
+  if (err) throw err;
+  console.log(result);
+  res.end(JSON.stringify(result));
+  db.close();
+});
+  // dbo.collection("kart").find({}).toArray(function(err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  //   res.end(JSON.stringify(result));
+  //   db.close();
+  // });
+});
+    }
+})
+
+app.post('/address/add', function (req, res) {
+var quser_id = req.body.user_id;
+var qproduct_id = req.body.product_id;
+var qqty = req.body.qty;
+
+var url = process.env.MONGODB_URI;
+    if(true){
+        MongoClient.connect(url, function(err, db) {
+          if (err) throw err;
+          var dbo = db.db("groceryapp");
+            var myobj = { product_id: qproduct_id, user_id: quser_id, qty: qqty };
+            dbo.collection("address").insertOne(myobj,function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.end(JSON.stringify(result));
+            db.close();
+          });
+        });
+        
+    }
+})
+
+app.post('/address/delete', function (req, res) {
+  var q_id = req.body._id;
+  var url = process.env.MONGODB_URI;
+  var myobj = { _id: new mongodb.ObjectId(q_id)};
+      if(true){
+          MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("groceryapp");
+              dbo.collection("address").deleteOne(myobj,function(err, result) {
+              if (err) throw err;
+              res.end(JSON.stringify(result));
+              db.close();
+            });
+          });         
+      }
+  })
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, err => {
     if(err) throw err;
